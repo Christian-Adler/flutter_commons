4e65fbdb84b4ddb28f3e50d04c5ebf2889e08195
Christian Adler <chris.adler@gmx.de>
2023-05-27 13:49:18 +0200

    add SafeAreaInfo static utils class

---
+++ lib/utils/safe_area_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SafeAreaInfo {
+  static EdgeInsets? _padding;
+
+  /// returns 0 if determine... is not yet called.
+  static double get safeAreaHeight {
+    final padding = _padding;
+    if (padding != null) return padding.top + padding.bottom;
+    return 0;
+  }
+
+  /// Has to be called in build before use of SafeArea - e.g. in the main App
+  static void determineSafeAreaHeight(BuildContext context) {
+    _padding = MediaQuery.of(context).padding;
+  }
+}
